74420dafb61170c9a825619ce9fe6bc4df437b17
Mellang <dlwpgh23@naver.com>
2021-06-15 23:49:47 +0900

    DB사용 준비

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth"; //auth구현시 임포트 필요
+import "firebase/database";
 
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <span>Home</span>;
+  const [mweet, setMweet] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setMweet(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="what's on your mind"
+        maaLength={120}
+        value={mweet}
+        onChange={onChange}
+      />
+      <input type="submit" value="Mweet" />
+    </form>
+  );
 };
 
 export default Home;
